00be7ca8e956e48ee82c0f6c52ef89a3bbf20c1d
merkanb <merkanb@gmail.com>
2023-12-11 20:11:12 +0100

    promises and day2 testing structure and command example

--- cypress/e2e/day01-Js-basic/promises.js
+++ cypress/e2e/day01-Js-basic/promises.js
@@ -1,5 +1,5 @@
-function getNumber (currency){
-    return new Promise((resolve, reject)=>{
+function getNumber(currency){
+    return new Promise((resolve,reject)=>{
         if(typeof currency !="number"){
             reject(new Error('currency is not a number'))
         }else{
@@ -9,5 +9,5 @@ function getNumber (currency){
 }
 
 getNumber("1000").then((val)=>{
-    console.log('val', val)
+    console.log('val',val)
 })
---
+++ cypress/e2e/day02/teststructure.js
@@ -0,0 +1,23 @@
+describe('First Test Suit', function(){
+    it('First Test Case', function(){
+        syntax
+    })
+    it('Seconnd Test Case', function(){
+        syntax
+    })
+    it('Third Test Case', function(){
+        syntax
+    })
+})
+
+describe('Second Test Suit', ()=>{
+    it('First Test Case',()=>{
+        syntax
+    })
+    it('Seconnd Test Case',()=>{
+        syntax
+    })
+    it('Third Test Case',()=>{
+        syntax
+    })
+})
