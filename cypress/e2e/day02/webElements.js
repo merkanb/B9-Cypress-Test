48210f1123cf2c800c294b511d987b0890c09378
merkanb <merkanb@gmail.com>
2024-06-02 01:38:15 +0200

    web element locating added

--- cypress/e2e/day02/webElements.js
+++ cypress/e2e/day02/webElements.js
@@ -6,19 +6,19 @@ describe('Web elements',()=>{         // test suit structure
         // By id
         cy.get("#loginpaage-input-email")
         // By className
-        cy.get('.led')
+        cy.get('.lead')
         cy.get('.large.test-primary')    // fill the blanks with .
         // By attribute name
         cy.get('[placeholder]')
-        // By attribute name and value
+        // By attribute name and value             // att. name and value all together may be unique !!! 
         cy.get('[placeholder="Email Address"]')
         // By tag name and attribute name and value
-        cy.get('input[placeholder="Email Address"]')
+        cy.get('input[placeholder="Email Address"]')         // this one also may be unique (with tag name)
         // by two different atttributes or more
-        cy.get('input[placeholder="Email Address"][name="email"]')
+        cy.get('input[placeholder="Email Address"][name="email"]')      //  2 or more att. names and values
         
-        cy.contains('Blue').click()
+        cy.contains('Blue').click()     //  ***   
 
 
     })
-})
+}) 
