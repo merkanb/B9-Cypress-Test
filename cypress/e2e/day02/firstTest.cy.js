9df7bfe363313ead7674afe647f55321e1959227
merkanb <merkanb@gmail.com>
2024-06-02 02:20:38 +0200

    google visited

--- cypress/e2e/day02/firstTest.cy.js
+++ cypress/e2e/day02/firstTest.cy.js
@@ -5,13 +5,14 @@ describe("first test suit",function(){
         cy.visit('https://www.google.com/')
         cy.get('#L2AGLb > .QS5gu').click()
         cy.get('#APjFqb').should('be.visible').type('cypress{enter}').wait(500)
+        cy.get('[lang="en"] > .tF2Cxc > .yuRUbf > :nth-child(1) > [jscontroller="msmzHf"] > a > .LC20lb')
+        .click().wait(500)
+        // cy.get('.tF2Cxc > .yuRUbf > :nth-child(1) > [jscontroller="msmzHf"] > a > .LC20lb').wait(1000).click()
+    
 
-        // cy.get('[lang="en"] > .tF2Cxc > .yuRUbf > :nth-child(1) > [jscontroller="msmzHf"] > a > .LC20lb').click()
-       
-
-        cy.get('[lang="en"] > .tF2Cxc > .yuRUbf > :nth-child(1) > [jscontroller="msmzHf"] > a > .notranslate > .GTRloc > .VuuXrf')
-.wait(500)
-.click()
+        //         cy.get('[lang="en"] > .tF2Cxc > .yuRUbf > :nth-child(1) > [jscontroller="msmzHf"] > a > .notranslate > .GTRloc > .VuuXrf')
+// .wait(500)
+// .click()
 
 
 
